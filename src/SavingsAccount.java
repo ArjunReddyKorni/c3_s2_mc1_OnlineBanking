cb2fa1419aaa621c18433dd16dc360e08b7c3db2
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-11 17:05:30 +0530

    Created the Methods.

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -14,6 +14,37 @@ public class SavingsAccount {
         this.status = status;
     }
 
+    public  String getAccountNumber(){
+        Random object = new Random();
+        String number = "";
+        for (int i = 0; i <12 ; i++)
+            number = number + object.nextInt(9);
+
+
+        return number;
+    }
+    public double withdraw(double withdrawedAmount){
+        if(withdrawedAmount < accountBalance){
+            accountBalance = accountBalance - withdrawedAmount;}
+        return accountBalance;
+    }
+    public double deposit(double deposit){
+
+        return accountBalance = accountBalance + deposit;
+    }
+    public double checkBalance(){
+
+        return accountBalance;
+    }
+    public String status(){
+
+        return status;
+    }
+    public double transfer(double amountToBeTransfered){
+        if(amountToBeTransfered < accountBalance)
+            accountBalance = accountBalance - amountToBeTransfered;
+        return accountBalance;
+    }
 
 
 }
